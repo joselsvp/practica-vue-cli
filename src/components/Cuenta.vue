58d591d5e97f53d4022677a5235614718f700143
Jose Villanueva <lv428694@gmail.com>
2022-05-16 22:17:36 -0500

    Update Cuenta.vue

--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1>Saldo:</h1>
+  <h1>Saldo:{{ saldo }}</h1>
+  <h1>Cuenta:{{ cuenta }}</h1>
+  <h1>Estado: {{ estado ? "Activada" : "Desactivada" }}</h1>
+  <h4>Servicios disponibles</h4>
+  <div v-for="(item, index) in servicios" :key="index">
+     {{index + 1}} - {{item}}
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      saldo: 1000,
+      cuenta: "Visa",
+      estado: true,
+      servicios: ["pagos", "giros", "transferencias"],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
